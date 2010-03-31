2e3258a16565ae6158bb3c71eedc25c94c143c5f
tim.teng <tim.rubist@gmail.com>
2010-03-31 18:04:33 +0800

    Version bump to 0.3.0

--- lib/wuid/ploymorphic_module.rb
+++ lib/wuid/ploymorphic_module.rb
@@ -9,44 +9,25 @@ module PloymorphicModule
     def acts_as_wuid options={}
       #TODO auto set wuid with conditional hook
       has_one :wuid, :as => :wuidable, :dependent => :destroy
-      if options[:acts_as_tree]
-        Wuid.class_eval do
-          acts_as_tree :foreign_key => "reference_to"
-        end
-        include PloymorphicModule::AdvancedInstanceMethods
-      else
-        include PloymorphicModule::InstanceMethods
-      end
+      attr_accessor :refs if options[:tree]
+      include PloymorphicModule::InstanceMethods
     end
 
   end
 
   module InstanceMethods
 
-    def after_create 
+    def wid
+      (self.wuid.parent) ? self.parent.wid : self.wuid.id
+    end
+
+    def after_create
       super
       create_wuid
-    end 
+    end
 
     def create_wuid
-      wuid.create
-    end
- 
-    def wid
-      self.wuid.id
-    end
-
-  end
-
-  module AdvancedInstanceMethods
-
-    def wid
-      (self.respond_to?(:reference_to) && self.parent) ? self.parent.wid : self.wuid.id
-    end
-
-    def wid= id=nil
-      w = self.wuild.build :reference_to => id
-      w.save 
+      wuid.create :reference_to => refs
     end
 
   end
--- lib/wuid/wuid.rb
+++ lib/wuid/wuid.rb
@@ -1,4 +1,5 @@
 class Wuid < ActiveRecord::Base
+  acts_as_tree :foreign_key => "reference_to"
   belongs_to :wuidable, :polymorphic => true
 end
 
